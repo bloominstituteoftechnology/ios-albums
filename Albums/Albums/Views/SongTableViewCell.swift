a392749e8ecd6d10771f94e4af668b7869be7051
Lisa Sampson <lisasampson0918@gmail.com>
2018-08-31 18:03:40 -0400

    Set up Storyboards/connect views and viewcontrollers

---
+++ Albums/Albums/Views/SongTableViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  SongTableViewCell.swift
+//  Albums
+//
+//  Created by Lisa Sampson on 8/31/18.
+//  Copyright © 2018 Lisa Sampson. All rights reserved.
+//
+
+import UIKit
+
+class SongTableViewCell: UITableViewCell {
+    
+    @IBAction func addSongButtonTapped(_ sender: Any) {
+        
+    }
+    
+    @IBOutlet weak var songLabel: UITextField!
+    @IBOutlet weak var durationLabel: UITextField!
+    @IBOutlet weak var addSongButton: UIButton!
+    
+}
