207b756fb8f5a0ed7ffe97f8fe98699a57c8de66
Lisa Sampson <lisasampson0918@gmail.com>
2018-09-02 23:20:28 -0400

    test decoder

--- Albums/Albums/Model Controller/AlbumController.swift
+++ Albums/Albums/Model Controller/AlbumController.swift
@@ -7,3 +7,38 @@
 //
 
 import Foundation
+
+class AlbumController {
+    
+    func testDecodingExampleAlbum() {
+        guard let url = Bundle.main.url(forResource: "exampleAlbum", withExtension: "json") else { print("URL is bad.") ; return }
+        
+        do {
+            let exampleData = try Data(contentsOf: url)
+            
+            let album = try JSONDecoder().decode(Album.self, from: exampleData)
+            
+            print("Success!")
+        }
+        catch {
+            print("Error getting data or decoding data: \(error)")
+        }
+    }
+    
+//    func textEncodingExampleAlbum() {
+//        guard let url = Bundle.main.url(forResource: "exampleAlbum", withExtension: "json") else { print("URL is bad.") ; return }
+//
+//        do {
+//            let exampleData = try Data(contentsOf: url)
+//
+//            let album = try JSONDecoder().decode(Album.self, from: exampleData)
+//
+//            _ = try JSONEncoder().encode(album)
+//
+//            print("Success!")
+//        }
+//        catch {
+//            print("Error getting data or decoding data: \(error)")
+//        }
+//    }
+}
