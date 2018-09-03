b8137dc171ed046a3035c15b996083d180b42a57
Lisa Sampson <lisasampson0918@gmail.com>
2018-09-03 13:22:06 -0400

    Configure table view cell

--- Albums/Albums/Views/SongTableViewCell.swift
+++ Albums/Albums/Views/SongTableViewCell.swift
@@ -8,10 +8,41 @@
 
 import UIKit
 
+protocol SongTableViewCellDelegate: class {
+    func addSong(with title: String, duration: String)
+}
+
 class SongTableViewCell: UITableViewCell {
     
     @IBAction func addSongButtonTapped(_ sender: Any) {
+        guard let songName = songLabel.text,
+            let duration = durationLabel.text else { return }
         
+        delegate?.addSong(with: songName, duration: duration)
+    }
+    
+    func updateViews() {
+        guard let song = song else { return }
+        
+        songLabel.text = song.songName
+        durationLabel.text = song.duration
+        addSongButton.isHidden = true
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        
+        songLabel.text = ""
+        durationLabel.text = ""
+        addSongButton.isHidden = false
+    }
+    
+    weak var delegate: SongTableViewCellDelegate?
+    
+    var song: Song? {
+        didSet {
+            updateViews()
+        }
     }
     
     @IBOutlet weak var songLabel: UITextField!
