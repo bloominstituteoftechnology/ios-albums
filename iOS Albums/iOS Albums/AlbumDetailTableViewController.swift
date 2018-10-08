62a7108cd80a1d5548d98244f7c3cfe0c603bb41
Dillon McElhinney <dillon.mce@gmail.com>
2018-10-08 08:46:36 -0600

    Add Xcode project. Lay out views and add view controllers.

---
+++ iOS Albums/iOS Albums/AlbumDetailTableViewController.swift
@@ -0,0 +1,53 @@
+//
+//  AlbumDetailTableViewController.swift
+//  iOS Albums
+//
+//  Created by Dillon McElhinney on 10/8/18.
+//  Copyright © 2018 Dillon McElhinney. All rights reserved.
+//
+
+import UIKit
+
+class AlbumDetailTableViewController: UITableViewController {
+
+    @IBOutlet weak var albumNameTextField: UITextField!
+    @IBOutlet weak var artistNameTextField: UITextField!
+    @IBOutlet weak var genresTextField: UITextField!
+    @IBOutlet weak var coverArtURLTextField: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func saveAlbum(_ sender: Any) {
+        
+    }
+    
+    // MARK: - Table View Data Source
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+
+    /*
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+}
---
+++ iOS Albums/iOS Albums/SongTableViewCell.swift
@@ -0,0 +1,22 @@
+//
+//  SongTableViewCell.swift
+//  iOS Albums
+//
+//  Created by Dillon McElhinney on 10/8/18.
+//  Copyright © 2018 Dillon McElhinney. All rights reserved.
+//
+
+import UIKit
+
+class SongTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var songTitleTextField: UITextField!
+    @IBOutlet weak var songDurationTextField: UITextField!
+    @IBOutlet weak var addSongButton: UIButton!
+    
+    
+    @IBAction func addSong(_ sender: Any) {
+        
+    }
+    
+}
