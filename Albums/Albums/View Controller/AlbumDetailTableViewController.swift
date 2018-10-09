2a2d1c61d5a2d28c21c0ca74bcb0bf9ee0b8452e
Ilqar Ilyasov <ilqar_ilyasov@yahoo.com>
2018-10-08 22:11:15 -0500

    heightForRowAt implemented

--- Albums/Albums/View Controller/AlbumDetailTableViewController.swift
+++ Albums/Albums/View Controller/AlbumDetailTableViewController.swift
@@ -12,6 +12,7 @@ class AlbumDetailTableViewController: UITableViewController, SongTableViewCellDe
     
     // MARK: - Properties
     
+    let reuseIdentifier = "SongTableCell"
     var albumController: AlbumController?
     var album: Album? { didSet { updateViews()}}
     var tempSongs: [Song] = []
@@ -65,25 +66,25 @@ class AlbumDetailTableViewController: UITableViewController, SongTableViewCellDe
     
     // MARK: - Table view data source
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return tempSongs.count + 1
     }
 
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath) as? SongTableViewCell else { return UITableViewCell() }
+        
+        cell.delegate = self
+        
         return cell
     }
-    */
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        if indexPath.section == 0 {
+            return UITableView.automaticDimension
+        } else {
+            return 140
+        }
+    }
 
     /*
     // Override to support conditional editing of the table view.
@@ -117,17 +118,6 @@ class AlbumDetailTableViewController: UITableViewController, SongTableViewCellDe
     override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
         // Return false if you do not want the item to be re-orderable.
         return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     }
+     */
 }
