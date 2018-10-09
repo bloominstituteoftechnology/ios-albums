a0715f1d5951c1aaa9d48f07c85b65e80f079dc4
Ilqar Ilyasov <ilqar_ilyasov@yahoo.com>
2018-10-08 19:11:53 -0500

    getAlbums func completed

--- Albums/Albums/Model Controller/AlbumController.swift
+++ Albums/Albums/Model Controller/AlbumController.swift
@@ -10,6 +10,53 @@ import Foundation
 
 class AlbumController {
     
+    // MARK: - Properties
+    
+    var album: [Album] = []
+    let baseURL = URL(string: "https://albums-3e830.firebaseio.com/")!
+    typealias Completion = (Error?) -> Void
+    
+    // MARK: - Get
+    
+    func getAlbums(completion: @escaping Completion) {
+        let url = baseURL.appendingPathExtension("json")
+        
+        URLSession.shared.dataTask(with: url) { (data, _, error) in
+            // Check error
+            // If error is not nil then log it
+            if error != nil {
+                NSLog("Error perfoeming data task")
+                completion(error)
+                return
+            }
+            
+            // Check data
+            // If data is nil then log it
+            guard let data = data else {
+                NSLog("No data returned")
+                return
+            }
+            
+            // If data is not nil then decode it
+            let decoder = JSONDecoder()
+            decoder.keyDecodingStrategy = .convertFromSnakeCase
+            do {
+                // Key String, Value Album
+                let albumDictionary = try decoder.decode([String:Album].self, from: data)
+                let values = albumDictionary.map( { $0.value }) // Get values
+                self.album = values
+            } catch {
+                NSLog("Error decoding data: \(error)")
+                completion(error)
+                return
+            }
+        }.resume()
+        
+    }
+    
+    
+    // MARK: - Testing
+
     static func testDecodingExampleAlbum() -> Album? {
         guard let url = Bundle.main.url(forResource: "exampleAlbum", withExtension: "json") else {
             NSLog("Json file doesn't exist")
@@ -40,4 +87,6 @@ class AlbumController {
             NSLog("Error encoding album")
         }
     }
+    
+    
 }
