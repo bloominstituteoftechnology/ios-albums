7889633c321b3e2ff07c707fce8fb7b769e9f338
Ilqar Ilyasov <ilqar_ilyasov@yahoo.com>
2018-10-08 21:49:25 -0500

    SongTableViewCellDelegate completed

--- Albums/Albums/View/SongTableViewCell.swift
+++ Albums/Albums/View/SongTableViewCell.swift
@@ -8,24 +8,47 @@
 
 import UIKit
 
+protocol SongTableViewCellDelegate: class {
+    func addSong(with title: String, duration: String)
+}
+
 class SongTableViewCell: UITableViewCell {
+    
+    // MARK: - Properties
+    
+    var song: Song? { didSet { updateViews() }}
+    weak var delegate: SongTableViewCellDelegate?
+    
+    // MARK: - Outlets
 
     @IBOutlet weak var songTitle: UITextField!
     @IBOutlet weak var songDuration: UITextField!
+    @IBOutlet weak var addSongButton: UIButton!
     
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
+    // MARK: - Actions
     
     @IBAction func addSongButtonTapped(_ sender: Any) {
+        guard let title = songTitle.text,
+            let duration = songDuration.text else { return }
+        
+        delegate?.addSong(with: title, duration: duration)
     }
     
-
+    // MARK: - Update
+    
+    func updateViews() {
+        if let song = song {
+            songTitle.text = song.name
+            songDuration.text = song.duration
+            addSongButton.isHidden = true
+        }
+    }
+    
+    // MARK: - Reuse the cell
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        songTitle.text = ""
+        songDuration.text = ""
+        addSongButton.isHidden = false
+    }
 }
