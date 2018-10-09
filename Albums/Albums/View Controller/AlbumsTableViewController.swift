c0f42968aa151cc5d26604af69b3a0a66fa060ac
Ilqar Ilyasov <ilqar_ilyasov@yahoo.com>
2018-10-08 21:33:55 -0500

    AlbumsTableVC Table view data source implemented

--- Albums/Albums/View Controller/AlbumsTableViewController.swift
+++ Albums/Albums/View Controller/AlbumsTableViewController.swift
@@ -9,38 +9,37 @@
 import UIKit
 
 class AlbumsTableViewController: UITableViewController {
+    
+    // MARK: - Properties
+    var albumController: AlbumController?
+    let reuseIdentifier = "AlbumTableCell"
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        albumController?.getAlbums(completion: { (error) in
+            if error != nil {
+                NSLog("Error fetching albums")
+            }
+            self.tableView.reloadData()
+        })
     }
 
     // MARK: - Table view data source
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return albumController?.albums.count ?? 1
     }
 
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath)
+        let album = albumController?.albums[indexPath.row]
+        
+        cell.textLabel?.text = album?.name
+        cell.detailTextLabel?.text = album?.artist
+        
         return cell
     }
-    */
 
     /*
     // Override to support conditional editing of the table view.
