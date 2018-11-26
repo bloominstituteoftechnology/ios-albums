4db46e0747dc42502cea2c3f47b425a3f9135cfb
Jerrick Warren <Jerrick.Warren@gmail.com>
2018-11-26 11:48:44 -0800

    inital commit/ project cereation

---
+++ ios-Albums/ios-Albums/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ios-Albums
+//
+//  Created by Jerrick Warren on 11/26/18.
+//  Copyright © 2018 Jerrick Warren. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
