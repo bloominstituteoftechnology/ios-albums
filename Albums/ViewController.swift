a2020461f15a8afa91917a72e875295c63b6106f
Lotanna <sugabelly@gmail.com>
2018-11-27 02:50:37 -0500

    storyboard done

---
+++ Albums/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Albums
+//
+//  Created by Lotanna Igwe-Odunze on 11/27/18.
+//  Copyright © 2018 Sugabelly LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
