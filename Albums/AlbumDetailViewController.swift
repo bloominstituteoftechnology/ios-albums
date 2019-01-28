17d6f64e9cc29b8af3efb0605f1f33063de5ae41
Lotanna <sugabelly@gmail.com>
2019-01-28 16:31:15 -0500

    added header view

---
+++ Albums/AlbumDetailViewController.swift
@@ -0,0 +1,14 @@
+//
+//  AlbumDetailViewController.swift
+//  Albums
+//
+//  Created by Lotanna Igwe-Odunze on 1/28/19.
+//  Copyright © 2019 Sugabelly LLC. All rights reserved.
+//
+
+import UIKit
+
+class AlbumDetailView: UITableViewController {
+    
+    
+}
