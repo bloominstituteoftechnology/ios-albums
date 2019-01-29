daff03ddf84f27f41a4d18c8a0feea4192cfe615
Sergey Osipyan <osipyansergey@gmail.com>
2019-01-28 18:59:29 -0800

    finished Part 2, decoder and encoder works fine

--- Albums/Albums/Album.swift
+++ Albums/Albums/Album.swift
@@ -12,7 +12,7 @@
 
 import Foundation
 
-struct Album: Decodable {
+struct Album: Codable {
     let artist: String
     let coverArt: [URL]
     let genres: [String]
@@ -26,12 +26,14 @@ struct Album: Decodable {
         case genres
         case id
         case name
+        case songs
+        
         enum CoverArt: String, CodingKey {
             case url
         }
     }
     
-    init(feom decoder: Decoder) throws {
+    init(from decoder: Decoder) throws {
         
         let container = try decoder.container(keyedBy: AlbumKeys.self)
         artist = try container.decode(String.self, forKey: .artist)
@@ -47,14 +49,17 @@ struct Album: Decodable {
             coverArtNames.append(urls)
         }
         }
+        
         coverArt = coverArtNames
+      //  coverArt = try container.decode([URL].self, forKey: .coverArt)
         genres = try container.decode([String].self, forKey: .genres)
-        songs = [try Song.init(from: Song.SongKeys.self as! Decoder)]
+        songs = try container.decode([Song].self, forKey: .songs)
+       
+}
     
-}
-}
 
-struct Song: Decodable {
+
+struct Song: Codable {
     let duration: String
     let id: String
     let name: String
@@ -68,7 +73,7 @@ struct Song: Decodable {
             case duration
         }
         enum Name: String, CodingKey {
-            case name
+            case title
         }
     }
     
@@ -78,11 +83,34 @@ struct Song: Decodable {
         let durationContainer = try container.nestedContainer(keyedBy: SongKeys.Duration.self, forKey: .duration)
         duration = try durationContainer.decode(String.self, forKey: .duration)
         let nameContainer = try container.nestedContainer(keyedBy: SongKeys.Name.self, forKey: .name)
-        name = try nameContainer.decode(String.self, forKey: .name)
+        name = try nameContainer.decode(String.self, forKey: .title)
     }
     
 }
+    func encode(to encoder: Encoder) throws {
+        
+        var albumContainer = encoder.container(keyedBy: AlbumKeys.self)
+        try albumContainer.encode(artist, forKey: .artist)
+        try albumContainer.encode(id, forKey: .id)
+        try albumContainer.encode(name, forKey: .name)
+        var coverArtContainer = albumContainer.nestedUnkeyedContainer(forKey: .coverArt)
+        
+       for art in coverArt {
+            try coverArtContainer.encode(art.absoluteString)
+            }
+         try albumContainer.encode(genres, forKey: .genres)
+        
+        var songContainer = albumContainer.nestedUnkeyedContainer(forKey: .songs)
+        for item in songs {
+            try songContainer.encode(item.duration)
+            try songContainer.encode(item.id)
+            try songContainer.encode(item.name)
+        }
+        try albumContainer.encode(genres, forKey: .genres)
+        
+    }
 
+}
 
 
 
--- Albums/Albums/AlbumController.swift
+++ Albums/Albums/AlbumController.swift
@@ -10,10 +10,37 @@ import Foundation
 
 class AlbumController {
     
-    func testDecodingExampleAlbum() {
+    var temp: Album?
+    
+    func testDecodingExampleAlbum() -> Album {
         
-        
-        
+        guard let url = Bundle.main.url(forResource: "exampleAlbum", withExtension: "json") else { print("URL is bad")
+            fatalError()
+        }
+        do {
+            let exampleAlbumData = try Data(contentsOf: url)
+            let album = try JSONDecoder().decode(Album.self, from: exampleAlbumData)
+            print(album)
+            temp = album
+            
+        } catch {
+            
+            print("error geting data \(error)")
+        }
+        return temp!
     }
     
+    func testEncodingExampleAlbum() {
+        
+       
+        do {
+        
+            let encodeAlbum = try JSONEncoder().encode(testDecodingExampleAlbum())
+            print(String(data: encodeAlbum, encoding: .utf8)!)
+        } catch {
+            
+             print("error encoding data \(error)")
+        }
+        
+    }
 }
