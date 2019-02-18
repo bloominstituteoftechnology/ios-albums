38fc9efad07ec6bfe871e6b1e14a811499457b01
Jocelyn Stuart <luluscomputer@jocelyns-macbook-air.local>
2019-02-18 15:21:33 -0500

    adds IB elements and  corresponding swift files

---
+++ Albums/Albums/SongTableViewCell.swift
@@ -0,0 +1,31 @@
+//
+//  SongTableViewCell.swift
+//  Albums
+//
+//  Created by Jocelyn Stuart on 2/18/19.
+//  Copyright © 2019 JS. All rights reserved.
+//
+
+import UIKit
+
+class SongTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    @IBAction func addSongTapped(_ sender: UIButton) {
+    }
+    
+    @IBOutlet weak var titleTextField: UITextField!
+    
+    @IBOutlet weak var durationTextField: UITextField!
+    
+
+}
---
+++ Albums/Albums/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Albums
+//
+//  Created by Jocelyn Stuart on 2/18/19.
+//  Copyright © 2019 JS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
