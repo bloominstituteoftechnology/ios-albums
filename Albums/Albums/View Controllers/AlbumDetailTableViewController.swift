a64e387f16514a7e0f5c08ec674a70911ac02993
Moses Robinson <mosesjrobinson@icloud.com>
2019-02-19 02:04:33 -0800

    application seems to be working, going to work on smothing it out

--- Albums/Albums/View Controllers/AlbumDetailTableViewController.swift
+++ Albums/Albums/View Controllers/AlbumDetailTableViewController.swift
@@ -8,44 +8,103 @@
 
 import UIKit
 
-class AlbumDetailTableViewController: UITableViewController {
-
+class AlbumDetailTableViewController: UITableViewController, SongTableViewCellDelegate {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        updateViews()
     }
     
     @IBAction func save(_ sender: Any) {
+        guard let artist = artistName.text, !artist.isEmpty,
+            let name = albumName.text, !name.isEmpty,
+            let genresString = genresTextField.text,
+            let coverArtString = coverArtTextField.text else { return }
         
+        let genres = genresString.split(separator: ",").map() { String($0) }
+        let coverArtArray = coverArtString.split(separator: ",").map() { String($0) }
+        let coverArt = coverArtArray.compactMap() { URL(string: $0) }
         
+        if let album = album {
+            albumController?.update(album: album, artist: artist, name: name, genres: genres, coverArt: coverArt, songs: tempSongs)
+        } else {
+            albumController?.createAlbum(artist: artist, name: name, genres: genres, coverArt: coverArt, songs: tempSongs)
+        }
+        navigationController?.popViewController(animated: true)
     }
-
+    
+    private func updateViews() {
+        if isViewLoaded {
+            guard let album = album else {
+                title = "New Album"
+                return }
+            
+            title = "Album:"
+            albumName.text = album.name
+            artistName.text = album.artist
+            genresTextField.text = album.genres.map({ $0 }).joined(separator: ", ")
+            coverArtTextField.text = album.coverArt.map({ $0.absoluteString }).joined(separator: ", ")
+            tempSongs = album.songs
+        }
+    }
+    
+    func addSong(with title: String, duration: String) {
+        guard let song = albumController?.createSong(name: title, duration: duration) else { return }
+        tempSongs.append(song)
+        tableView.reloadData()
+        
+        let indexPath = IndexPath(row: tempSongs.count, section: 0)
+        tableView.scrollToRow(at: indexPath, at: .none, animated: true)
+    }
+    
     // MARK: - Table view data source
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-       
-        return 1
+        
+        return tempSongs.count + 1
     }
     
     let reuseIdentifier = "SongCell"
-
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath) as! SongTableViewCell
-
-        // Configure the cell...
-
+        
+        if indexPath.row < tempSongs.count {
+            let song = tempSongs[indexPath.row]
+            cell.song = song
+        }
+        cell.delegate = self
+        cell.selectionStyle = .none
+        
         return cell
     }
-
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        if indexPath.row < tempSongs.count {
+            return 90
+        } else {
+            return 130
+        }
+    }
+    
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            
             
             
             tableView.deleteRows(at: [indexPath], with: .fade)
         }
     }
- 
+    
     // MARK: - Properties
+    
+    var albumController: AlbumController?
+    var album: Album? {
+        didSet {
+            updateViews()
+        }
+    }
+    
+    var tempSongs: [Song] = []
     
     @IBOutlet var albumName: UITextField!
     @IBOutlet var artistName: UITextField!
--- Albums/Albums/View Controllers/AlbumsTableViewController.swift
+++ Albums/Albums/View Controllers/AlbumsTableViewController.swift
@@ -9,28 +9,39 @@
 import UIKit
 
 class AlbumsTableViewController: UITableViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        albumController.getAlbums(completion: { (error) in
+            if let error = error {
+                NSLog("Error getting albums: \(error)")
+                return
+            }
+            self.tableView.reloadData()
+        })
     }
-
+    
     // MARK: - Table view data source
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-
-        return 1
+        
+        return albumController.albums.count
     }
-
+    
     let reuseIdentifier = "AlbumCell"
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath)
-
-        // Configure the cell...
-
+        
+        let album = albumController.albums[indexPath.row]
+        
+        cell.textLabel?.text = album.name
+        cell.detailTextLabel?.text = album.artist
+        cell.selectionStyle = .none
+        
         return cell
     }
-
+    
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             
@@ -39,16 +50,26 @@ class AlbumsTableViewController: UITableViewController {
             tableView.deleteRows(at: [indexPath], with: .fade)
         }
     }
-
+    
     // MARK: - Navigation
-
-   
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        
-        // SEGUES HERE
-        
+        if segue.identifier == "ShowCreateAlbum" {
+            guard let destination = segue.destination as? AlbumDetailTableViewController else { return }
+            
+            destination.albumController = albumController
+        } else if segue.identifier == "ShowAlbumDetail" {
+            guard let destination = segue.destination as? AlbumDetailTableViewController,
+                let indexPath = tableView.indexPathForSelectedRow else { return }
+            let album = albumController.albums[indexPath.row]
+            
+            destination.albumController = albumController
+            destination.album = album
+        }
     }
-
+    
     // MARK: - Properties
-
+    
+    let albumController = AlbumController()
+    
 }
