2cbff9d82427650d44894a1a2a6d6f6f9d48f9a6
Paul Yi <yipaulc@gmail.com>
2019-02-25 20:50:16 -0800

    Create testDecodingExampleAlbum() method

---
+++ Albums/Albums/Model Controller/AlbumController.swift
@@ -0,0 +1,30 @@
+//
+//  AlbumController.swift
+//  Albums
+//
+//  Created by Paul Yi on 2/25/19.
+//  Copyright © 2019 Paul Yi. All rights reserved.
+//
+
+import Foundation
+
+class AlbumController {
+    
+    func testDecodingExampleAlbum() {
+        guard let url = Bundle.main.url(forResource: "exampleAlbum", withExtension: "json") else { print("URL is bad.") ;
+            return
+        }
+        
+        do {
+            let exampleData = try Data(contentsOf: url)
+            
+            _ = try JSONDecoder().decode(Album.self, from: exampleData)
+            
+            print("Success!")
+        }
+        catch {
+            print("Error getting data or decoding date: \(error)")
+        }
+        
+    }
+}
