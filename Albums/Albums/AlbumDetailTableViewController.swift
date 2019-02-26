a30a7742920bbd44114759ec90bb04ae07bbca38
nateyoungren <nathanaelyoungren@gmail.com>
2019-02-25 18:29:12 -0800

    Wire views.

--- Albums/Albums/AlbumDetailTableViewController.swift
+++ Albums/Albums/AlbumDetailTableViewController.swift
@@ -8,14 +8,63 @@
 
 import UIKit
 
-class AlbumDetailTableViewController: UITableViewController {
+class AlbumDetailTableViewController: UITableViewController, SongTableViewCellDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        updateViews()
     }
     
     @IBAction func saveButtonTapped(_ sender: UIBarButtonItem) {
+        guard let title = titleTextField.text,
+        let artist = artistTextField.text,
+        let genres = genresTextField.text,
+            let urls = urlsTextField.text else { return }
         
+        if let album = album {
+            albumController?.update(album: album, artist: artist, coverArt: URL(string: "\(urls)"), genres: genres, id: album.id, name: title)
+        } else {
+            albumController?.createAlbum(artist: artist, coverArt: URL(string: "\(urls)"), genres: genres, name: title, songs: tempSongs)
+        }
+        navigationController?.popViewController(animated: true)
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tempSongs.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "SongCell", for: indexPath)
+        guard let songCell = cell as? SongTableViewCell else { return cell }
+        songCell.delegate = self
+        return songCell
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 140
+    }
+    
+    private func updateViews() {
+        guard let album = album else {
+            navigationItem.title = "New Album"
+            return }
+        navigationItem.title = album.name
+        titleTextField.text = album.name
+        artistTextField.text = album.artist
+        genresTextField.text = album.genres.joined(separator: ", ")
+        urlsTextField.text = album.coverArt?.absoluteString
+        if let songs = album.songs {
+            tempSongs = songs
+        }
+    }
+    
+    func addSong(with title: String, duration: String) {
+        let song = albumController?.createSong(name: title, duration: duration)
+        tempSongs.append(song!)
+        tableView.reloadData()
+        
+        let index = IndexPath(row: album!.songs!.count, section: 0)
+        tableView.scrollToRow(at: index, at: .middle, animated: true)
     }
     
     @IBOutlet weak var titleTextField: UITextField!
@@ -24,5 +73,10 @@ class AlbumDetailTableViewController: UITableViewController {
     @IBOutlet weak var urlsTextField: UITextField!
     
     var albumController: AlbumController?
-    var album: Album?
+    var album: Album? {
+        didSet {
+            updateViews()
+        }
+    }
+    var tempSongs: [Song] = []
 }
