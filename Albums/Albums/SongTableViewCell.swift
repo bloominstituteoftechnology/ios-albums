a9a9b8898a97f6a6ce852ece9dbc6526474e4c96
nateyoungren <nathanaelyoungren@gmail.com>
2019-02-25 17:47:12 -0800

    Set up song detail cell.

--- Albums/Albums/SongTableViewCell.swift
+++ Albums/Albums/SongTableViewCell.swift
@@ -8,13 +8,34 @@
 
 import UIKit
 
+protocol SongTableViewCellDelegate: class {
+    func addSong(with title: String, duration: String)
+}
+
 class SongTableViewCell: UITableViewCell {
 
     @IBAction func addSongButtonTapped(_ sender: UIButton) {
-        
+        guard let title = songTitleTextField.text,
+            let duration = songDurationTextField.text else { return }
+        delegate?.addSong(with: title, duration: duration)
+    }
+    
+    func updateViews() {
+        guard let song = song else { return }
+        songTitleTextField.text = song.name
+        songDurationTextField.text = song.duration
+        addSongButton.isHidden = true
+    }
+    
+    override func prepareForReuse() {
+        songTitleTextField.text = ""
+        songDurationTextField.text = ""
+        addSongButton.isHidden = false
     }
     
     @IBOutlet weak var songTitleTextField: UITextField!
     @IBOutlet weak var songDurationTextField: UITextField!
     @IBOutlet weak var addSongButton: UIButton!
+    var song: Song?
+    weak var delegate: SongTableViewCellDelegate?
 }
