e89406213ca0a17c2a502ac5e8f4e7c6b7712eda
Michael Redig <mredig@gmail.com>
2019-06-03 16:24:00 -0500

    added starter project

---
+++ An Album Cover/An Album Cover/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  An Album Cover
+//
+//  Created by Michael Redig on 6/3/19.
+//  Copyright © 2019 Red_Egg Productions. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
