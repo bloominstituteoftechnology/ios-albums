dd3d6b48c843d254f37e7e1962844237862801e6
hectorsvill <hectorstevenvillasano@gmail.com>
2019-06-03 15:38:36 -0700

    AlbumsTableViewController

---
+++ albums/albums/AlbumsTableViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  AlbumsTableViewCell.swift
+//  albums
+//
+//  Created by Hector Steven on 6/3/19.
+//  Copyright © 2019 Hector Steven. All rights reserved.
+//
+
+import UIKit
+
+class AlbumsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+
+}
---
+++ albums/albums/AlbumsTableViewController.swift
@@ -0,0 +1,20 @@
+//
+//  AlbumsTableViewController.swift
+//  albums
+//
+//  Created by Hector Steven on 6/3/19.
+//  Copyright © 2019 Hector Steven. All rights reserved.
+//
+
+import UIKit
+
+class AlbumsTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+	}
+	
+	
+	
+
+}
