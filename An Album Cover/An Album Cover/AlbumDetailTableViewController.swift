7a504abc1b0da93ab02e026c3a5bdb2bec169dff
Michael Redig <mredig@gmail.com>
2019-06-03 23:59:28 -0500

    progress setting up vcs

--- An Album Cover/An Album Cover/AlbumDetailTableViewController.swift
+++ An Album Cover/An Album Cover/AlbumDetailTableViewController.swift
@@ -13,4 +13,33 @@ class AlbumDetailTableViewController: UITableViewController {
 	@IBOutlet var artistNameTextField: UITextField!
 	@IBOutlet var genresTextField: UITextField!
 	@IBOutlet var coverArtTextField: UITextField!
+
+	var anAlbumController: AnAlbumController?
+	var album: AnAlbum? {
+		didSet {
+			updateViews()
+		}
+	}
+
+	private func updateViews() {
+		guard let album = album else { return }
+		albumNameTextField.text = album.name
+		artistNameTextField.text = album.artist
+		genresTextField.text = album.genres.joined(separator: ", ")
+		coverArtTextField.text = album.coverArt.map { $0.absoluteString }.joined(separator: ", ")
+
+		tableView.reloadData()
+	}
+}
+
+extension AlbumDetailTableViewController {
+	override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return album?.songs.count ?? 0
+	}
+
+	override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		let cell = tableView.dequeueReusableCell(withIdentifier: "SongCell", for: indexPath)
+
+		return cell
+	}
 }
--- An Album Cover/An Album Cover/AlbumsTableViewController.swift
+++ An Album Cover/An Album Cover/AlbumsTableViewController.swift
@@ -14,8 +14,33 @@ class AlbumsTableViewController: UITableViewController {
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
-//		anAlbumController.testDecodingExampleAlbum()
-//		anAlbumController.testEncodingExampleAlbum()
+		anAlbumController.getAlbums { [weak self] (result: Result<[AnAlbum], NetworkError>) in
+			self?.tableView.reloadData()
+		}
 	}
 
+	override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+		if let dest = segue.destination as? AlbumDetailTableViewController {
+			dest.anAlbumController = anAlbumController
+
+			if segue.identifier == "ShowAlbumDetail" {
+				guard let indexPath = tableView.indexPathForSelectedRow else { return }
+				dest.album = anAlbumController.albums[indexPath.row]
+			}
+		}
+	}
+}
+
+extension AlbumsTableViewController {
+	override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return anAlbumController.albums.count
+	}
+
+	override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+		let album = anAlbumController.albums[indexPath.row]
+		cell.textLabel?.text = album.name
+		cell.detailTextLabel?.text = album.artist
+		return cell
+	}
 }
