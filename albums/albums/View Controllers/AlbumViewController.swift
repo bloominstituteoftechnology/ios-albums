43e1ce079ee04c0a54feaf4fdf5f155f035192de
hectorsvill <hectorstevenvillasano@gmail.com>
2019-06-04 17:54:31 -0700

    UITableViewDataSource

--- albums/albums/View Controllers/AlbumViewController.swift
+++ albums/albums/View Controllers/AlbumViewController.swift
@@ -8,10 +8,21 @@
 
 import UIKit
 
-class AlbumViewController: UIViewController {
+class AlbumViewController: UIViewController, UITableViewDataSource {
+	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return 10
+	}
+	
+	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		let cell = songsTableView.dequeueReusableCell(withIdentifier: "songCell", for: indexPath)
+		cell.textLabel?.text = "1"
+		return cell
+	}
+	
 
     override func viewDidLoad() {
         super.viewDidLoad()
+		songsTableView.dataSource = self
 		saveRightBarButtonItem()
 	}
 	
