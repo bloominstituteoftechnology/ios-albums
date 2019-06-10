02ec3786e3b93fc67fcdb3ddedb1db590b449eca
hectorsvill <hectorstevenvillasano@gmail.com>
2019-06-10 14:27:21 -0700

    sections

--- Albums/Albums/View Controllers/AlbumDetailTableViewController.swift
+++ Albums/Albums/View Controllers/AlbumDetailTableViewController.swift
@@ -15,14 +15,22 @@ class AlbumDetailTableViewController: UITableViewController {
 
 	}
 	
+	override func numberOfSections(in tableView: UITableView) -> Int {
+		return 2
+	}
+	
 	override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-		return 10
+		if section == 1 {
+			return 1
+		}
+		return 3
 	}
 	override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
 		let cell = tableView.dequeueReusableCell(withIdentifier: "albumDetailCell", for: indexPath)
 		guard let albumDetailCell = cell as? AlbumDetailTableViewCell else { return cell }
-		
-		
+		if indexPath.section == 0 {
+			albumDetailCell.addSongButtonOutlet.isHidden = true
+		}
 		return albumDetailCell
 	}
 	
