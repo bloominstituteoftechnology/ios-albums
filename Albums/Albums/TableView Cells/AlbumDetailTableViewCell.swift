30bd67705b401a72d94aaaf8ddaa3c19b53dbaad
hectorsvill <hectorstevenvillasano@gmail.com>
2019-06-10 13:37:16 -0700

    AlbumDetailTableViewCell

---
+++ Albums/Albums/TableView Cells/AlbumDetailTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  AlbumDetailTableViewCell.swift
+//  Albums
+//
+//  Created by Hector Steven on 6/10/19.
+//  Copyright © 2019 Hector Steven. All rights reserved.
+//
+
+import UIKit
+
+class AlbumDetailTableViewCell: UITableViewCell {
+
+	@IBAction func AddSongButtonPressed(_ sender: Any) {
+	
+	}
+	
+	
+	
+	@IBOutlet var songNameTextField: UITextField!
+	@IBOutlet var songDurationTextField: UITextField!
+	
+	@IBOutlet var addSongButtonOutlet: UIButton!
+	
+}
