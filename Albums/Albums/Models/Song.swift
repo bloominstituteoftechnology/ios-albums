d8daa5a6874b94e8f8ab1a2946539c6a3a912b8b
hectorsvill <hectorstevenvillasano@gmail.com>
2019-06-10 17:23:42 -0700

    NameCodingKeys:

--- Albums/Albums/Models/Song.swift
+++ Albums/Albums/Models/Song.swift
@@ -10,14 +10,26 @@ import Foundation
 
 struct Song: Codable {
 	let id: String
+	let name: String
 	
 	enum CodingKeys: String, CodingKey {
 		case id
+		case name
+		
+		
+		enum NameCodingKeys: String, CodingKey {
+			case title
+		}
+		
 	}
 	
 	init(from decoder: Decoder) throws {
 		let container = try decoder.container(keyedBy: CodingKeys.self)
 		id = try container.decode(String.self, forKey: .id)
+		
+		let nameContainer = try container.nestedContainer(keyedBy: CodingKeys.NameCodingKeys.self, forKey: .name)
+		name = try nameContainer.decode(String.self, forKey: .title)
+		
 		
 	}
 	
