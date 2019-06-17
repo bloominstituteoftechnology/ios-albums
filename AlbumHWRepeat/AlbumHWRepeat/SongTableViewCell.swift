d18e17d6ffeeb82802a54e2a63c1d8c71ae40fb2
Michael R.F <mrflowers22@uwalumni.com>
2019-06-17 13:49:47 -0700

    created the story board and attached source code file to each applicable scenes.

---
+++ AlbumHWRepeat/AlbumHWRepeat/SongTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  SongTableViewCell.swift
+//  AlbumHWRepeat
+//
+//  Created by Michael Flowers on 6/17/19.
+//  Copyright © 2019 Michael Flowers. All rights reserved.
+//
+
+import UIKit
+
+class SongTableViewCell: UITableViewCell {
+
+   
+
+}
