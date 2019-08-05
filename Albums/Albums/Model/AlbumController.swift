49f45c84a751f6ec4f0f5e890cc0b11f7fbb36ff
Kat Milton <mltnkat@gmail.com>
2019-08-05 13:40:13 -0700

    add album model.

---
+++ Albums/Albums/Model/AlbumController.swift
@@ -0,0 +1,9 @@
+//
+//  AlbumController.swift
+//  Albums
+//
+//  Created by Kat Milton on 8/5/19.
+//  Copyright © 2019 Kat Milton. All rights reserved.
+//
+
+import Foundation
---
+++ Albums/Albums/Model/Song.swift
@@ -0,0 +1,51 @@
+//
+//  Song.swift
+//  Albums
+//
+//  Created by Kat Milton on 8/5/19.
+//  Copyright © 2019 Kat Milton. All rights reserved.
+//
+
+import Foundation
+
+struct Song: Equatable, Codable {
+    let id: UUID
+    let duration: String
+    let name: String
+    
+    enum CodingKeys: String, CodingKey {
+        case id
+        case duration
+        case name
+        
+        enum DurationKeys: String, CodingKey {
+            case duration
+        }
+        
+        enum NameKeys: String, CodingKey {
+            case title
+        }
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        
+        let idString = try container.decode(String.self, forKey: .id)
+        guard let tID = UUID(uuidString: idString) else { throw NSError() }
+        id = tID
+        let durationContainer = try container.nestedContainer(keyedBy: CodingKeys.DurationKeys.self, forKey: .duration)
+        duration = try durationContainer.decode(String.self, forKey: .duration)
+        let nameContainer = try container.nestedContainer(keyedBy: CodingKeys.NameKeys.self, forKey: .name)
+        name = try nameContainer.decode(String.self, forKey: .title)
+    }
+    
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        
+        try container.encode(id.uuidString, forKey: .id)
+        var durationContainer = container.nestedContainer(keyedBy: CodingKeys.DurationKeys.self, forKey: .duration)
+        try durationContainer.encode(duration, forKey: .duration)
+        var nameContainer = container.nestedContainer(keyedBy: CodingKeys.NameKeys.self, forKey: .name)
+        try nameContainer.encode(name, forKey: .title)
+    }
+}
