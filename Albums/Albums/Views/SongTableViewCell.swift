efa6c4b09fd06248db5b7e0a4a78326f2c547ea1
Kat Milton <mltnkat@gmail.com>
2019-08-05 13:08:58 -0700

    add all storyboard views, outlets, and actions.

---
+++ Albums/Albums/Views/SongTableViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  SongTableViewCell.swift
+//  Albums
+//
+//  Created by Kat Milton on 8/5/19.
+//  Copyright © 2019 Kat Milton. All rights reserved.
+//
+
+import UIKit
+
+class SongTableViewCell: UITableViewCell {
+    
+    @IBOutlet var songTitleTextField: UITextField!
+    @IBOutlet var songDurationTextField: UITextField!
+
+    @IBAction func addSongButtonPressed(_ sender: UIButton) {
+        
+        
+    }
+
+}
