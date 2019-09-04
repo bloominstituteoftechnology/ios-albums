16b4be724ce05508aca66f966c78be95bab31ab4
bradleyyin <bradleyyin@gmail.com>
2019-09-03 20:46:37 -0700

    implement song table view cell

--- Album/Album/View/SongTableViewCell.swift
+++ Album/Album/View/SongTableViewCell.swift
@@ -8,13 +8,39 @@
 
 import UIKit
 
+protocol SongTableViewCellDelegate {
+    func addSong(with title: String, duration: String)
+}
+
 class SongTableViewCell: UITableViewCell {
     
     @IBOutlet weak var titleLabel: UITextField!
     @IBOutlet weak var durationLabel: UITextField!
+    @IBOutlet weak var addSongButton: UIButton!
     
-    @IBOutlet weak var addSongTapped: UIButton!
+    var song: Song? {
+        didSet{
+            updateViews()
+        }
+    }
+    var delegate: SongTableViewCellDelegate?
     
+    func updateViews() {
+        guard let song = song else { return }
+        titleLabel.text = song.title
+        durationLabel.text = song.duration
+        addSongButton.isHidden = true
+    }
     
-
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        titleLabel.text = nil
+        durationLabel.text = nil
+        addSongButton.isHidden = false
+    }
+    @IBAction func addSongTapped(_ sender: Any) {
+        guard let title = titleLabel.text, !title.isEmpty, let duration = durationLabel.text, !duration.isEmpty else { return }
+        delegate?.addSong(with: title, duration: duration)
+    }
+    
 }
