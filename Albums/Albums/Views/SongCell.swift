7b04f44aaa03ec85f43d11108815ab3db6fed825
Jeffrey <SantanaJny@gmail.com>
2019-09-04 23:46:41 -0400

    Linking outlets/buttons, updating UI

--- Albums/Albums/Views/SongCell.swift
+++ Albums/Albums/Views/SongCell.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 protocol SongCellDelegate {
-	func newSongAdded()
+	func newSongAdded(_ newSong: Song)
 }
 
 class SongCell: UITableViewCell {
@@ -18,17 +18,36 @@ class SongCell: UITableViewCell {
 	
 	@IBOutlet weak var titleTextField: UITextField!
 	@IBOutlet weak var durationTextField: UITextField!
+	@IBOutlet weak var addSongBtn: UIButton!
 	
 	//MARK: - Properties
 	
+	var delegate: SongCellDelegate?
+	var song: Song? {
+		didSet {
+			configCell()
+		}
+	}
 	
 	//MARK: - IBActions
 	
 	@IBAction func addSongBtnTapped(_ sender: Any) {
+		guard let title = titleTextField.optionalText, let duration = durationTextField.optionalText else { return }
+		let newSong = Song(id: nil, title: title, duration: duration)
 		
+		delegate?.newSongAdded(newSong)
 	}
 	
 	//MARK: - Helpers
 	
+	private func configCell() {
+		guard let song = song else { return }
+		titleTextField.text = song.title
+		durationTextField.text = song.duration
+		
+		titleTextField.isEnabled = false
+		durationTextField.isEnabled = false
+		addSongBtn.isHidden = true
+	}
 	
 }
