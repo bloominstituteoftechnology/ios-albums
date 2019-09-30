acc9c1e9dcc32543a2e59fdb1375e160d2f6ec0a
Jake <jlconnerly@gmail.com>
2019-09-30 14:39:57 -0500

    Completed UI setup

---
+++ Albums/Albums/View Controllers/AlbumDetailTableViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AlbumDetailTableViewController.swift
+//  Albums
+//
+//  Created by Jake Connerly on 9/30/19.
+//  Copyright © 2019 jake connerly. All rights reserved.
+//
+
+import UIKit
+
+class AlbumDetailTableViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
