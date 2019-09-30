15d5ab0a8b14f22c10f962655c4046d2822da290
ladybeitel <ciarabeitel@gmail.com>
2019-09-30 16:38:29 -0400

    Set up project. Complete Part 1 - Storyboard Setup

---
+++ Albums/Albums/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Albums
+//
+//  Created by Ciara Beitel on 9/30/19.
+//  Copyright © 2019 Ciara Beitel. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Albums/Albums/Views/SongTableViewCell.swift
@@ -0,0 +1,31 @@
+//
+//  SongTableViewCell.swift
+//  Albums
+//
+//  Created by Ciara Beitel on 9/30/19.
+//  Copyright © 2019 Ciara Beitel. All rights reserved.
+//
+
+import UIKit
+
+class SongTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var songTitle: UITextField!
+    @IBOutlet weak var songDuration: UITextField!
+    
+    
+    @IBAction func addSongButton(_ sender: UIButton) {
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
