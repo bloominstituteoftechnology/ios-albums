09dcc01973feb3f1763af48029b77164f5ade053
bfvilchez <bfvilchez@icloud.com>
2019-09-30 15:15:52 -0600

    created an xcode project

---
+++ Albums/Albums/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Albums
+//
+//  Created by brian vilchez on 9/30/19.
+//  Copyright © 2019 brian vilchez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
