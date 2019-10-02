7edef2f6cd6106493981f0aabdad810830122e5a
Percy <percival.ngan@gmail.com>
2019-10-02 08:52:39 -0500

    Added encode function to Album model

--- Albums/Albums/Album.swift
+++ Albums/Albums/Album.swift
@@ -10,24 +10,33 @@ import Foundation
 
 
 struct Album: Codable {
+	let id: String
 	let name: String
 	let artist: String
-	let genres: [String]
-	let coverArt: [URL]
+	var genres: [String]
+	let coverArt: URL
 	let songs: [Song]
 
 	enum AlbumKeys: String, CodingKey {
+		case id
 		case name
 		case artist
 		case genres
 		case coverArt
 		case songs
+
+		enum CoverArtCodingKeys: String, CodingKey {
+			case url
+		}
 	}
+
+
 
 	init(from decoder: Decoder) throws {
 
 		let container = try decoder.container(keyedBy: AlbumKeys.self)
 
+		id = try container.decode(String.self, forKey: .id)
 		name = try container.decode(String.self, forKey: .name)
 		artist = try container.decode(String.self, forKey: .artist)
 
@@ -38,8 +47,10 @@ struct Album: Codable {
 			genreNames.append(genreString)
 		}
 		genres = genreNames
+		genres = try container.decode([String].self, forKey: .genres)
 
 		var coverArtContainer = try container.nestedUnkeyedContainer(forKey: .coverArt)
+		let urlContainer = try coverArtContainer.nestedContainer(keyedBy: AlbumKeys.CoverArtCodingKeys.self)
 		var coverArtUrl: [URL] = []
 		while !coverArtContainer.isAtEnd {
 			let coverArtString = try coverArtContainer.decode(String.self)
@@ -47,9 +58,23 @@ struct Album: Codable {
 				coverArtUrl.append(url)
 			}
 		}
-		coverArt = coverArtUrl
+		coverArt = try urlContainer.decode(URL.self, forKey: .url)
 
-		songs = try container.decode([Song], forKey: .songs)
+		songs = try container.decode([Song].self, forKey: .songs)
+	}
+
+	func encode(encoder: Encoder) throws {
+
+		var container = encoder.container(keyedBy: AlbumKeys.self)
+		var coverArtContainer = container.nestedUnkeyedContainer(forKey: .coverArt)
+		var urlContainer = coverArtContainer.nestedContainer(keyedBy: AlbumKeys.CoverArtCodingKeys.self)
+		try container.encode(artist, forKey: .artist)
+		try urlContainer.encode(coverArt.absoluteString, forKey: .url)
+		try container.encode(id, forKey: .id)
+		try container.encode(name, forKey: .name)
+		try container.encode(songs, forKey: .songs)
+		try container.encode(genres, forKey: .genres)
+
 	}
 }
 
