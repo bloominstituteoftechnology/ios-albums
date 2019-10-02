3c85b1b4983b97f3958297f1e56d270229b1ae66
ladybeitel <ciarabeitel@gmail.com>
2019-10-01 21:32:39 -0400

    Complete Part 4 - Wiring Up The Views, SongTableViewCell, Steps 1-6

--- Albums/Albums/Views/SongTableViewCell.swift
+++ Albums/Albums/Views/SongTableViewCell.swift
@@ -10,21 +10,45 @@ import UIKit
 
 class SongTableViewCell: UITableViewCell {
     
+    var song: Song?
+    var delegate: SongTableViewCellDelegate?
+    
     @IBOutlet weak var songTitle: UITextField!
     @IBOutlet weak var songDuration: UITextField!
+    @IBOutlet weak var addSong: UIButton!
+    
        
     @IBAction func addSongButton(_ sender: UIButton) {
+        guard let songTitle = songTitle.text,
+            let songDuration = songDuration.text else { return }
+        self.delegate?.addSong(with: songTitle, duration: songDuration)
+    }
+    
+    func updateViews() {
+        if let song = song {
+            songTitle.text = song.name
+            songDuration.text = song.duration
+            addSong.isHidden = true
+        } else {
+            addSong.isHidden = false
+        }
+    }
+    
+    override func prepareForReuse() {
+        songTitle.text?.removeAll()
+        songDuration.text?.removeAll()
+        addSong.isHidden = false
     }
 
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
     }
+}
 
+protocol SongTableViewCellDelegate {
+    func addSong(with title: String, duration: String)
 }
