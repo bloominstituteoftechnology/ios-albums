1e3b4a14c273402637c9a86f1947b03a4001a752
Jonalynn Masters <jonalynna.masters@gmail.com>
2019-10-28 14:28:48 -0700

    Initial Commit

---
+++ Albums/Albums/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Albums
+//
+//  Created by Jonalynn Masters on 10/28/19.
+//  Copyright © 2019 Jonalynn Masters. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
