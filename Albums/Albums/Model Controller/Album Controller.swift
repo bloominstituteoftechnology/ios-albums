a7f4ca64b894da93db57c156ce39007401170848
Jonalynn Masters <jonalynna.masters@gmail.com>
2019-10-28 15:46:09 -0700

    Decoding Test function created and ran in the AppDelegate (no errors)

--- Albums/Albums/Model Controller/Album Controller.swift
+++ Albums/Albums/Model Controller/Album Controller.swift
@@ -7,3 +7,23 @@
 //
 
 import Foundation
+
+class AlbumController {
+    var albums: [Album] = []
+    var songs: [Songs] = []
+    
+// MARK: Test Decoding Example Album
+    func testDecodingExampleAlbum() {
+        
+        let mainBundle = Bundle.main
+        let fileURL = mainBundle.url(forResource: "exampleAlbum", withExtension: "json")!
+        
+        do {
+            let json = try Data(contentsOf: fileURL)
+            let album = try JSONDecoder().decode(Album.self, from: json)
+            print("\(album)")
+        } catch {
+            NSLog("Error loading test data: \(error)")
+        }
+    }
+}
