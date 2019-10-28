9282727a98df3fc88eb8bd882a44263bb9fc679a
Alex Shillingford <alex.shillingford@icloud.com>
2019-10-28 13:40:52 -0600

    Create Xcode Project; First Commit

---
+++ Albums/Albums/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Albums
+//
+//  Created by Alex Shillingford on 10/28/19.
+//  Copyright © 2019 Alex Shillingford. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
