5308f980740f3801a78830140d4d677ec2ac65d1
Jonalynn Masters <jonalynna.masters@gmail.com>
2019-10-28 14:42:50 -0700

    Storyboard set up; files created

---
+++ Albums/Albums/Model Controller/Album Controller.swift
@@ -0,0 +1,9 @@
+//
+//  Album Controller.swift
+//  Albums
+//
+//  Created by Jonalynn Masters on 10/28/19.
+//  Copyright © 2019 Jonalynn Masters. All rights reserved.
+//
+
+import Foundation
---
+++ Albums/Albums/ViewControllers/SongTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  SongTableViewCell.swift
+//  Albums
+//
+//  Created by Jonalynn Masters on 10/28/19.
+//  Copyright © 2019 Jonalynn Masters. All rights reserved.
+//
+
+import UIKit
+
+class SongTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
