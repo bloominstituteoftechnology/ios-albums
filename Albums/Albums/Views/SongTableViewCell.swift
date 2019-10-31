2fdaa56c3d90a5144338ae493812752e8d5981d9
John Kouris <jdkouris@gmail.com>
2019-10-30 18:35:48 -0700

    Finished initial design and added necessary code files with outlets/actions

---
+++ Albums/Albums/Views/SongTableViewCell.swift
@@ -0,0 +1,33 @@
+//
+//  SongTableViewCell.swift
+//  Albums
+//
+//  Created by John Kouris on 10/30/19.
+//  Copyright © 2019 John Kouris. All rights reserved.
+//
+
+import UIKit
+
+class SongTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var songTitleTextField: UITextField!
+    @IBOutlet weak var songDurationTextField: UITextField!
+    @IBOutlet weak var addSongButton: UIButton!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    @IBAction func addSongButtonTapped(_ sender: Any) {
+        
+    }
+    
+
+}
