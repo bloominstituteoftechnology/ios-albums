86a9c675231070d20a3fd583fff0139d712047cc
brian <bfvilchez95@gmail.com>
2019-12-03 06:57:51 -0700

    still working on project

---
+++ Albums/Albums/Album.swift
@@ -0,0 +1,13 @@
+//
+//  Album.swift
+//  Albums
+//
+//  Created by brian vilchez on 12/3/19.
+//  Copyright © 2019 brian vilchez. All rights reserved.
+//
+
+import Foundation
+
+struct Album {
+    
+}
