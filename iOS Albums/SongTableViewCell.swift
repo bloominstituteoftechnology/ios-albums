610430790f42f93e7dd5691570b984607a4d31d6
Christian Lorenzo <clbmiami2004@gmail.com>
2020-01-15 21:49:00 -0800

    Created MainStoryBoard. Added Controllers and outlets.

---
+++ iOS Albums/SongTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  SongTableViewCell.swift
+//  iOS Albums
+//
+//  Created by Lambda_School_Loaner_201 on 1/15/20.
+//  Copyright © 2020 Christian Lorenzo. All rights reserved.
+//
+
+import UIKit
+
+class SongTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ iOS Albums/iOS Albums/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOS Albums
+//
+//  Created by Lambda_School_Loaner_201 on 1/15/20.
+//  Copyright © 2020 Christian Lorenzo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
