acc16f051395a1ba0856ee789aa615c988ef6f90
Rwoltx <richardx1016@gmail.com>
2020-01-16 10:48:56 -0500

    Model

---
+++ Albums/Albums/Album.swift
@@ -0,0 +1,17 @@
+//
+//  Album.swift
+//  Albums
+//
+//  Created by Rick Wolter on 1/15/20.
+//  Copyright © 2020 Devshop7. All rights reserved.
+//
+
+import Foundation
+
+struct Album {
+    
+    
+    
+    
+    
+}
