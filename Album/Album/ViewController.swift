33aa4af00c58bb06e3bd64e169c9edb43296997a
CrystalKnightCodes <CrystalKnightCodes@gmail.com>
2020-01-16 19:12:17 -0500

    Initial Commit

---
+++ Album/Album/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Album
+//
+//  Created by Christy Hicks on 1/16/20.
+//  Copyright © 2020 Knight Night. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
