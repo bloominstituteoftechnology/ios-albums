58583838abab1caa1f732e15c12044c53d0b6a18
Zack Larsen <zacklarsen1@gmail.com>
2020-02-10 13:11:16 -0700

    finished part 1

---
+++ Albums/Albums/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Albums
+//
+//  Created by Zack Larsen on 2/10/20.
+//  Copyright © 2020 Zack Larsen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
