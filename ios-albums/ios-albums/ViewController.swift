08fa120454d292c7f1eda8f1c07a688056422235
Ezra Black <dev.ezrablack@gmail.com>
2020-03-09 14:36:05 -0500

    Base Project Added To Master

---
+++ ios-albums/ios-albums/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ios-albums
+//
+//  Created by Joseph Rogers on 3/9/20.
+//  Copyright © 2020 Casanova Studios. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
