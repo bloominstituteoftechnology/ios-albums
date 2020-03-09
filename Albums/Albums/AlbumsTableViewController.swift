2fe624477ba4994c185960109b4e669f7702fa0a
Enrique Gongora <EGTech24@gmail.com>
2020-03-09 12:32:19 -0700

    Storyboard Setup complete

---
+++ Albums/Albums/AlbumsTableViewController.swift
@@ -0,0 +1,26 @@
+//
+//  AlbumsTableViewController.swift
+//  Albums
+//
+//  Created by Enrique Gongora on 3/9/20.
+//  Copyright © 2020 Enrique Gongora. All rights reserved.
+//
+
+import UIKit
+
+class AlbumsTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+}
