516c596230e6fe64dafbc0be523efd0f1688b107
Jessie <jessie.griffin@icloud.com>
2020-03-11 21:27:15 -0400

    Adds SongCell and Title and Duration textFields

---
+++ Albums/Albums/Views/SongTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  SongTableViewCell.swift
+//  Albums
+//
+//  Created by Jessie Ann Griffin on 3/11/20.
+//  Copyright © 2020 Jessie Griffin. All rights reserved.
+//
+
+import UIKit
+
+class SongTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
