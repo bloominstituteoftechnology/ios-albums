aaba5f241f62b1f160306a41e50a00877fb065f8
David Wright <davidtwright89@gmail.com>
2020-03-12 02:00:30 -0400

    Finished Part 2: Created model objects and manually implemented Codable protocol on both

---
+++ Albums/Albums/Models/Album.swift
@@ -0,0 +1,79 @@
+//
+//  Album.swift
+//  Albums
+//
+//  Created by David Wright on 3/11/20.
+//  Copyright © 2020 David Wright. All rights reserved.
+//
+
+import Foundation
+
+struct Album {
+    
+    // MARK: - Properties
+
+    let artist: String
+    var coverArtURLs: [URL]
+    let genres: [String]
+    let id: String
+    let name: String
+    let songs: [Song]
+    
+    enum AlbumKeys: String, CodingKey {
+        case id
+        case name
+        case artist
+        case coverArt
+        case genres
+        case songs
+        
+        enum CoverArtKeys: String, CodingKey {
+            case url
+        }
+    }
+}
+
+extension Album: Codable {
+    
+    init(from decoder: Decoder) throws {
+        
+        // { full json }
+        let jsonContainer = try decoder.container(keyedBy: AlbumKeys.self)
+        
+        self.artist = try jsonContainer.decode(String.self, forKey: .artist)
+        self.genres = try jsonContainer.decode([String].self, forKey: .genres)
+        self.id = try jsonContainer.decode(String.self, forKey: .id)
+        self.name = try jsonContainer.decode(String.self, forKey: .name)
+        self.songs = try jsonContainer.decode([Song].self, forKey: .songs)
+        self.coverArtURLs = []
+        
+        // "coverArt": [...]
+        var coverArtUnkeyedContainer = try jsonContainer.nestedUnkeyedContainer(forKey: .coverArt)
+        
+        while !coverArtUnkeyedContainer.isAtEnd {
+            // { "url": ... }
+            let coverArtKeyedContainer = try coverArtUnkeyedContainer.nestedContainer(keyedBy: AlbumKeys.CoverArtKeys.self)
+            
+            // "url" for the first value inside coverArt
+            let url = try coverArtKeyedContainer.decode(URL.self, forKey: .url)
+            coverArtURLs.append(url)
+        }
+    }
+    
+    func encode(to encoder: Encoder) throws {
+        var jsonContainer = encoder.container(keyedBy: AlbumKeys.self)
+        
+        try! jsonContainer.encode(self.id, forKey: .id)
+        try! jsonContainer.encode(self.name, forKey: .name)
+        try! jsonContainer.encode(self.artist, forKey: .artist)
+        try! jsonContainer.encode(self.genres, forKey: .genres)
+        try! jsonContainer.encode(self.songs, forKey: .songs)
+        
+        var coverArtUnkeyedContainer = jsonContainer.nestedUnkeyedContainer(forKey: .coverArt)
+        var coverArtKeyedContainer = coverArtUnkeyedContainer.nestedContainer(keyedBy: AlbumKeys.CoverArtKeys.self)
+                
+        for url in coverArtURLs {
+            try! coverArtKeyedContainer.encode(url, forKey: .url)
+        }
+    }
+}
---
+++ Albums/Albums/Models/Song.swift
@@ -0,0 +1,56 @@
+//
+//  Song.swift
+//  Albums
+//
+//  Created by David Wright on 3/11/20.
+//  Copyright © 2020 David Wright. All rights reserved.
+//
+
+import Foundation
+
+struct Song {
+    let duration: String
+    let id: String
+    let title: String
+    
+    enum SongKeys: String, CodingKey {
+        case duration
+        case id
+        case name
+        
+        enum DurationKeys: String, CodingKey {
+            case duration
+        }
+        
+        enum NameKeys: String, CodingKey {
+            case title
+        }
+    }
+}
+
+extension Song: Codable {
+    
+    init(from decoder: Decoder) throws {
+        let jsonContainer = try decoder.container(keyedBy: SongKeys.self)
+        
+        let durationKeyedContainer = try jsonContainer.nestedContainer(keyedBy: SongKeys.DurationKeys.self, forKey: .duration)
+        
+        let nameKeyedContainer = try jsonContainer.nestedContainer(keyedBy: SongKeys.NameKeys.self, forKey: .name)
+        
+        self.duration = try durationKeyedContainer.decode(String.self, forKey: .duration)
+        self.id = try jsonContainer.decode(String.self, forKey: .id)
+        self.title = try nameKeyedContainer.decode(String.self, forKey: .title)
+    }
+    
+    func encode(to encoder: Encoder) throws {
+        var jsonContainer = encoder.container(keyedBy: SongKeys.self)
+        
+        var durationKeyedContainer = jsonContainer.nestedContainer(keyedBy: SongKeys.DurationKeys.self, forKey: .duration)
+               
+        var nameKeyedContainer = jsonContainer.nestedContainer(keyedBy: SongKeys.NameKeys.self, forKey: .name)
+        
+        try! jsonContainer.encode(self.id, forKey: .id)
+        try! durationKeyedContainer.encode(self.duration, forKey: .duration)
+        try! nameKeyedContainer.encode(self.title, forKey: .title)
+    }
+}
