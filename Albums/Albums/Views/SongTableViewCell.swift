2cb870ebade4c0f5d7914f154fb3faa6b3decb2e
Jessie <jessie.griffin@icloud.com>
2020-03-13 22:43:01 -0400

    Adds protocol and button functionality

--- Albums/Albums/Views/SongTableViewCell.swift
+++ Albums/Albums/Views/SongTableViewCell.swift
@@ -8,12 +8,18 @@
 
 import UIKit
 
+protocol SongTableViewCellDelegate {
+    func addSong(with title: String, duration: String)
+}
+
 class SongTableViewCell: UITableViewCell {
 
     @IBOutlet weak var songTitleTextField: UITextField!
     @IBOutlet weak var songDurationTextField: UITextField!
-    
     @IBOutlet weak var addSongButton: UIButton!
+    
+    var song: Song?
+    weak var delegate: SongTableViewCellDelegate?
     
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -27,5 +33,22 @@ class SongTableViewCell: UITableViewCell {
     }
 
     @IBAction func addSongButtonPressed(_ sender: UIButton) {
+        guard let songTitle = songTitleTextField.text,
+            let duration = songDurationTextField.text,
+            !songTitle.isEmpty,
+            !duration.isEmpty else { return }
+        delegate?.addSong(with: songTitle, duration: duration)
+    }
+    
+    func updateViews() {
+        if let song = song {
+            songTitleTextField.text = song.name
+            songDurationTextField.text = song.duration
+            addSongButton.isHidden = true
+        }
+    }
+    
+    override func prepareForReuse() {
+        <#code#>
     }
 }
