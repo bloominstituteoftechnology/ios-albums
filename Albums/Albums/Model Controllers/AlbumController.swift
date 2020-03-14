dca4ab093ae0704caba943fc9bf38614be04f4e0
Jessie <jessie.griffin@icloud.com>
2020-03-13 21:54:14 -0400

    create, update, put methods added

--- Albums/Albums/Model Controllers/AlbumController.swift
+++ Albums/Albums/Model Controllers/AlbumController.swift
@@ -14,6 +14,44 @@ class AlbumController {
     
     private let baseURL = URL(string: "https://lambdaalbumscodableproject.firebaseio.com/")
     
+    func createAlbum(artist: String, coverArt: [URL], genres:[String], id: String, name: String, songs: [Song]) {
+        let album = Album(artist: artist, coverArt: coverArt, genres: genres, id: id, name: name, songs: songs)
+        albums.append(album)
+        put(album: album)
+    }
+    
+    func createSong(id: String, name: String, duration: String) -> Song {
+        let song = Song(id: id, name: name, duration: duration)
+        return song
+    }
+    
+    func update(album: Album, artist: String, coverArt: [URL], genres:[String], id: String, name: String, songs: [Song]) {
+        
+        guard let index = albums.firstIndex(of: album) else { return }
+        
+        albums[index].artist = artist
+        albums[index].coverArt = coverArt
+        albums[index].genres = genres
+        albums[index].name = name
+        albums[index].songs = songs
+        put(album: album)
+    }
+    
+    func put(album: Album, completion: @escaping (Error?) -> Void = { _ in }) {
+        guard let baseURL = baseURL else { return }
+        let requestURL = baseURL.appendingPathComponent(album.id).appendingPathExtension("json")
+        var request = URLRequest(url: requestURL)
+        request.httpMethod = "PUT"
+        
+        URLSession.shared.dataTask(with: request) { data, _, error in
+            guard error == nil else {
+                print("Error PUTing album to the server: \(error!)")
+                completion(error)
+                return
+            }
+        }.resume()
+    }
+    
     func getAlbums(completion: @escaping (Error?) -> Void = { _ in }) {
         guard let baseURL = baseURL else { return }
         let requestURL = baseURL.appendingPathExtension("json")
@@ -24,21 +62,25 @@ class AlbumController {
                 completion(error)
                 return
             }
-            
+
             guard let data = data else {
                 print("No data returned")
                 completion(NSError())
                 return
             }
-            
+
             do {
-                
+                let decodedAlbums = Array(try JSONDecoder().decode([String : Album].self, from: data).values)
+                self.albums = decodedAlbums
+                completion(nil)
             } catch {
-                
+                print("Error decoding album: \(error)")
+                completion(error)
+                return
             }
-        }
-        
+        }.resume()
     }
+    
     func testDecodingExampleAlbum() {
         
         let urlPath = Bundle.main.url(forResource: "exampleAlbum", withExtension: "json")!
