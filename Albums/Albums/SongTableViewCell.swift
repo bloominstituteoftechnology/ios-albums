ea8aa65b922975ed3b0079a56ba8316bc8280ca7
Shawn Gee <shawngee@gmail.com>
2020-04-06 18:28:24 -0400

    Song cell set up

--- Albums/Albums/SongTableViewCell.swift
+++ Albums/Albums/SongTableViewCell.swift
@@ -9,11 +9,32 @@
 import UIKit
 
 class SongTableViewCell: UITableViewCell {
+    
+    // MARK: - Public Properties
+    
+    var song: Song? { didSet { updateViews() }}
+
     // MARK: - IBOutlets
     @IBOutlet weak var titleTextField: UITextField!
     @IBOutlet weak var durationTextField: UITextField!
+    @IBOutlet weak var addSongButton: UIButton!
     
     // MARK: - IBActions
     @IBAction func addSongTapped(_ sender: UIButton) {
+    }
+    
+    // MARK: - Private
+    
+    private func updateViews() {
+        guard let song = song else { return }
+        titleTextField.text = song.title
+        durationTextField.text = song.duration
+        addSongButton.isHidden = true
+    }
+    
+    override func prepareForReuse() {
+        titleTextField.text = nil
+        durationTextField.text = nil
+        addSongButton.isHidden = false
     }
 }
