2827b42650cd8fe8806bd79117f639c4f0c03132
Mark Gerrior <github@gerrior.com>
2020-04-06 13:40:17 -0700

    AlbumDetailViewController complete

---
+++ Albums/Albums/SongTableViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  SongTableViewCell.swift
+//  Albums
+//
+//  Created by Mark Gerrior on 4/6/20.
+//  Copyright © 2020 Mark Gerrior. All rights reserved.
+//
+
+import UIKit
+
+class SongTableViewCell: UITableViewCell {
+
+    // MARK: - Actions
+    
+    @IBAction func addSongButton(_ sender: Any) {
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
