7f45c0701db777a33e513523106cbd4505930703
Wyatt Harrell <wyattaharrell@gmail.com>
2020-04-06 15:37:54 -0400

    new project created

---
+++ Albums/Albums/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Albums
+//
+//  Created by Wyatt Harrell on 4/6/20.
+//  Copyright © 2020 Wyatt Harrell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
