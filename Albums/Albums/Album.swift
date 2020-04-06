19d6670c25f2608b2c1069487d54d55ff2569165
Mark Gerrior <github@gerrior.com>
2020-04-06 14:11:30 -0700

    Part 2. Step 2

--- Albums/Albums/Album.swift
+++ Albums/Albums/Album.swift
@@ -8,6 +8,17 @@
 
 import Foundation
 
-struct Album: Codable {
-    
+struct Album: Codable: Decodable {
+    var id: UUID        // 5E58FA0F-7DBD-4F1D-956F-89756CF1EB22
+    var artist: String  // Weezer
+    var coverArt: [URL] // https://lastfm-img2.akamaized.net/i/u/174s/1918fe81bb68441d96b2247682bfda21.png
+    var genres: [String] // Alternative
+    var name: String    // Weezer (The Blue Album)
+    var song: [Song]
+}
+
+struct Song: Decodable {
+    var id: UUID
+    var title: String
+    var duration: String
 }
