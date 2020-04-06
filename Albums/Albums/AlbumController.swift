e08e3abb8b585e1528e6344b99ee1043db08a9b7
Mark Gerrior <github@gerrior.com>
2020-04-06 15:16:47 -0700

    Add AlbumController. Step 5

---
+++ Albums/Albums/AlbumController.swift
@@ -0,0 +1,13 @@
+//
+//  AlbumController.swift
+//  Albums
+//
+//  Created by Mark Gerrior on 4/6/20.
+//  Copyright © 2020 Mark Gerrior. All rights reserved.
+//
+
+import Foundation
+
+class AlbumController {
+    
+}
