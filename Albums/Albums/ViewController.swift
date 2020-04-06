3abe9bba3589611722cc7d0a80027e3b977ed636
Mark Gerrior <github@gerrior.com>
2020-04-06 12:23:01 -0700

    Add Album Project

---
+++ Albums/Albums/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Albums
+//
+//  Created by Mark Gerrior on 4/6/20.
+//  Copyright © 2020 Mark Gerrior. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
