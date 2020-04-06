9b1bc0d16fbff983ef341e705ce040d2d4c06c47
Hector L <hectoremil21@hotmail.com>
2020-04-06 18:34:48 -0400

    Encoding works and pretty prints

--- Albums/Albums/Controllers/AlbumController.swift
+++ Albums/Albums/Controllers/AlbumController.swift
@@ -26,9 +26,25 @@ class AlbumController {
         } catch {
             print(error)
         }
+    }
+    
+    func testEncodingExampleAlbum() {
         
+        let urlPath = Bundle.main.url(forResource: "exampleAlbum", withExtension: "json")!
+        let codedData = try! Data(contentsOf: urlPath)
         
+        let decoder = JSONDecoder()
         
+        let encoder = JSONEncoder()
+        encoder.outputFormatting = .prettyPrinted
         
+        do {
+            let data = try decoder.decode(Album.self, from: codedData)
+            let encoded = try encoder.encode(data)
+            let dataAsString = String(data: encoded, encoding: .utf8)!
+            print(dataAsString)
+        } catch {
+            print(error)
+        }
     }
 }
