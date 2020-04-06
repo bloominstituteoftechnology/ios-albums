cffe74695c8604705c5e2eaa5681517282fae3d5
Brad <braddiroff@icloud.com>
2020-04-06 15:35:43 -0400

    Created project

---
+++ AlbumList/AlbumList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AlbumList
+//
+//  Created by Bradley Diroff on 4/6/20.
+//  Copyright © 2020 Bradley Diroff. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
