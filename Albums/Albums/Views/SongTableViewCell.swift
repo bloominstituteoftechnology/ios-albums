e8e118fb1b43d23b01170b08b280aa9f8deb9b39
Cody Morley <codymorleyinc@gmail.com>
2020-04-10 00:11:04 -0700

    finished part 1, storyboard setup

---
+++ Albums/Albums/Views/SongTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  SongTableViewCell.swift
+//  Albums
+//
+//  Created by Bling Morley on 4/10/20.
+//  Copyright © 2020 Cody Morley. All rights reserved.
+//
+
+import UIKit
+
+class SongTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
