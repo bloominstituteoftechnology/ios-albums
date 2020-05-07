12360ae0d7995ddd8babac7653e99fc98ae4c9c9
Nonye <nonye@Nonyes-MacBook-Air.local>
2020-05-07 11:48:12 -0700

    Intial Commit, Storyboard set up

---
+++ Albums/Albums/Album.swift
@@ -0,0 +1,13 @@
+//
+//  Album.swift
+//  Albums
+//
+//  Created by Nonye on 5/7/20.
+//  Copyright © 2020 Nonye Ezekwo. All rights reserved.
+//
+
+import Foundation
+
+struct Album: Codable {
+    
+}
