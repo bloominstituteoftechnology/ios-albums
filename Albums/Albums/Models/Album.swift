e99d672d14e53ffecb4d85769a8c758a937a2d10
Cody Morley <codymorleyinc@gmail.com>
2020-05-07 16:02:33 -0700

    still working on model inits.

--- Albums/Albums/Models/Album.swift
+++ Albums/Albums/Models/Album.swift
@@ -12,14 +12,35 @@ struct Album: Decodable {
     enum CodingKeys: String, CodingKey {
         case title = "name"
         case id, artist, coverArt, genres, songs
+        
+        enum CoverKeys: String, CodingKey {
+            case url
+        }
     }
     
-    let title: String
-    let artist: String
-    let id: String
-    let genres: [String]
-    let coverArt: [URL]
-    let songs: [Song]
+    var title: String
+    var artist: String
+    var id: String
+    var genres: [String]
+    var coverArt: [URL]
+    var songs: [Song]
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        
+        title = try container.decode(String.self, forKey: .title)
+        artist = try container.decode(String.self, forKey: .artist)
+        id = try container.decode(String.self, forKey: .id)
+        
+        var genresContainer = try container.nestedUnkeyedContainer(forKey: .genres)
+        genres = try genresContainer.decode([String].self)
+        
+        var coverArtContainer = try container.nestedContainer(keyedBy: CodingKeys.CoverKeys.self, forKey: .coverArt)
+        coverArt = try coverArtContainer.decode([URL].self, forKey: .url)
+        
+        var songsContainer = try container.nestedUnkeyedContainer(forKey: <#T##KeyedDecodingContainer<CodingKeys>.Key#>)
+        
+    }
 }
 
 struct Song: Decodable {
@@ -29,11 +50,15 @@ struct Song: Decodable {
         case id
     }
     
-    let title: String
-    let duration: String
-    let id: String
+    var title: String
+    var duration: String
+    var id: String
     
     init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
         
+        title = try container.decode(String.self, forKey: .title)
+        duration = try container.decode(String.self, forKey: .duration)
+        id = try container.decode(String.self, forKey: .id)
     }
 }
