84c916319d7746186016ed511dcd9bc6c7a336b9
Kenneth Jones <kennethjones1991@outlook.com>
2020-05-18 17:28:31 -0600

    Set up AlbumController outline

--- Albums/AlbumController.swift
+++ Albums/AlbumController.swift
@@ -9,6 +9,11 @@
 import Foundation
 
 class AlbumController {
+    
+    var albums: [Album] = []
+    
+    private let baseURL = URL(string: "https://console.firebase.google.com/u/0/project/albums-f0adc/database/firestore/data~2F")!
+    
     func testDecodingExampleAlbum() {
         guard let urlPath = Bundle.main.url(forResource: "exampleAlbum", withExtension: "json") else { return }
         do {
@@ -36,5 +41,25 @@ class AlbumController {
         } catch {
             NSLog("Error coding test album: \(error)")
         }
+    }
+    
+    func getAlbums(completion: @escaping (Error?) -> ()) {
+        
+    }
+    
+    func putAlbum(album: Album) {
+        
+    }
+    
+    func createAlbum(name: String, artist: String, genres: [String], coverArt: [URL], songs: [Song]) {
+        
+    }
+    
+    func createSong(name: String, duration: String) -> Song {
+        
+    }
+    
+    func update(album: Album, name: String, artist: String, genres: [String], coverArt: [URL], songs: [Song]) {
+        
     }
 }
